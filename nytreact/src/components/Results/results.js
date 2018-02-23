0389f520e776d8b4d774eb659f06fff0381f49ad
Michael Morfessis <Michael.Morfessis@globalinformationsystems.com>
2018-02-23 13:52:25 -0600

    Updated

--- nytreact/src/components/Results/results.js
+++ nytreact/src/components/Results/results.js
@@ -3,7 +3,7 @@ import React from "react";
 const Results = props => (
     <div>
         <div><span>{props.title}</span> {props.snippet} </div>
-        <span>{props.link}</span><a href={props.url}> Read Article </a>
+        <span>{props.link}</span><a href={props.url} target="_blank"> Read Article </a>
         <div><span>{props.text}</span> {props.date} </div>
         <br />
     </div>
